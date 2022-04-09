e74bdd1d1db490d671a65aad90105ed88db11422
Auroiah Morgan <amwebdev@outlook.com>
2022-04-09 00:09:58 -0400

    Update Container.js

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,10 +1,11 @@
-import  Header  from '../Header/Header.js';
+import Header from '../Header/Header.js';
 
 
 export default function Container(props) {
   return (
-    <div className='container'>
+
+    < div className='container' >
       <Header />
-    </div>
+    </div >
   );
 }
