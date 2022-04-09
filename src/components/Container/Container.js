2119264ff2135a1b86addb50a805928d3a341153
Auroiah Morgan <amwebdev@outlook.com>
2022-04-08 23:43:28 -0400

    Update component structure
    Puts Container in its own folder

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,4 +1,4 @@
-import  Header  from './components/Header/Header.js';
+import  Header  from '../Header/Header.js';
 
 
 export default function Container(props) {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import  Container  from './Container';
+import  Container  from './components/Container/Container';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
