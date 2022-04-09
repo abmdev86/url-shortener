7207ac0790aca3ce4b9355b05c112c52d1a8ff3e
Auroiah Morgan <amwebdev@outlook.com>
2022-04-09 00:01:16 -0400

    Update ErrorBoundary.js

--- src/components/ErrorBoundary/ErrorBoundary.js
+++ src/components/ErrorBoundary/ErrorBoundary.js
@@ -1,10 +1,11 @@
 
 import React from 'react';
+import FallbackUI from '../FallbackUI/FallbackUI';
 
 export default class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { hasError: false };
+    this.state = { hasError: false, errorInfo: "Testing the information" };
   }
 
   static getDerivedStateFromError(error) {
@@ -13,13 +14,13 @@ export default class ErrorBoundary extends React.Component {
   }
   componentDidCatch(error, errorInfo) {
     // You can also log the error to an error reporting service
-
+    return { errorInfo: errorInfo };
   }
 
   render() {
     if (this.state.hasError) {
       // You can render any custom fallback UI
-      return <h1>Something went wrong.</h1>;
+      return <FallbackUI errorMsg={this.errorInfo} />;
     }
     return this.props.children;
   }
