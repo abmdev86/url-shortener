83e656b1a5e7e405a87e16ea368a66cdf0ef1740
Auroiah Morgan <amwebdev@outlook.com>
2022-04-09 20:27:49 -0400

    Update Container.js

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,11 +1,12 @@
 import Header from '../Header/Header.js';
-
+import URLInputComponent from '../URLComponent/URLInputComponent'
 
 export default function Container(props) {
   return (
 
     < div className='container' >
       <Header />
+      <URLInputComponent/>
     </div >
   );
 }
