fc6ca0c205d255e66598979a43b9e4fd301aace2
Auroiah Morgan <amwebdev@outlook.com>
2022-04-18 17:30:00 -0400

    Update CopyToClipboardComponent.js

--- src/components/CopyToClipboard/CopyToClipboardComponent.js
+++ src/components/CopyToClipboard/CopyToClipboardComponent.js
@@ -1,64 +1,44 @@
-import { useState } from 'react';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
-
-
+import { useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 function CopyToClipboardComponent(props) {
-
   const [isCopied, setIsCopied] = useState(false);
 
   function copy() {
     setIsCopied(true);
-    props.text = ' ';
-
-
+    props.text = " ";
   }
 
   if (!isCopied && props.text) {
     return (
-
       <div>
-
-
-
-        <CopyToClipboard text={props.text} onCopy={() => copy()} options={{ debug: props.debug, message: "" }} style={props.style}  >
+        <CopyToClipboard
+          text={props.text}
+          onCopy={() => copy()}
+          options={{ debug: props.debug, message: "" }}
+          style={props.style}
+        >
           <button>Copy</button>
         </CopyToClipboard>
-
-
-
       </div>
-
     );
-
   } else if (isCopied && props.text) {
-
-    return (
-      <span>Copied!</span>
-    );
-
+    return <span>Copied!</span>;
   } else if (isCopied && !props.text) {
     setIsCopied(false);
     return (
-
       <div>
-
-
-
-        <CopyToClipboard text={props.text} onCopy={() => copy()} options={{ debug: props.debug, message: "" }} style={props.style}  >
+        <CopyToClipboard
+          text={props.text}
+          onCopy={() => copy()}
+          options={{ debug: props.debug, message: "" }}
+          style={props.style}
+        >
           <button>Copy</button>
         </CopyToClipboard>
-
-
-
       </div>
-
     );
   }
-
-
-
-
 }
 
 export default CopyToClipboardComponent;
