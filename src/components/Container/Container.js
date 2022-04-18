638de27de5db2822ab60e92acc8230d9cceb152c
Auroiah Morgan <amwebdev@outlook.com>
2022-04-18 18:05:07 -0400

    Update Container.js

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,13 +1,20 @@
-import Header from '../Header/Header.js';
-import URLComponent from '../URLComponent/URLComponent.js';
+import Header from "../Header/Header.js";
+import URLComponent from "../URLComponent/URLComponent.js";
+import { Col, Row, Container } from "react-bootstrap";
 
-
-export default function Container(props) {
+export default function AppContainer(props) {
   return (
-
-    < div className='container' >
-      <Header />
-      <URLComponent />
-    </div >
+    <Container fluid sm md lg xl xxl>
+      <Row className="justify-content-md-center">
+        <Col xs md lg>
+          <Header />
+        </Col>
+      </Row>
+      <Row className="justify-content-md-center">
+        <Col xs={true} lg={true}>
+          <URLComponent />
+        </Col>
+      </Row>
+    </Container>
   );
 }
