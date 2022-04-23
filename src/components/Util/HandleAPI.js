a0b3696e8935713c1f4ce3b7c1a81d049ff493e0
Auroiah Morgan <amwebdev@outlook.com>
2022-04-23 15:58:02 -0400

    Update logger

--- src/components/Util/HandleAPI.js
+++ src/components/Util/HandleAPI.js
@@ -1,49 +1,48 @@
 import env from "react-dotenv";
-
-const SHORTEN_API = 'https://api-ssl.bitly.com/v4/shorten';
+import { logger } from "./LoggerUtil";
+const SHORTEN_API = "https://api-ssl.bitly.com/v4/shorten";
 const TOKEN = env.BITLY_KEY;
 
 async function getShortenURL_POST(url) {
+  try {
+    var postDataObject = { long_url: url };
+    var token = TOKEN;
 
-  var postDataObject = { long_url: url };
-  var token = TOKEN;
+    async function getLink() {
+      var link = await postURLData(SHORTEN_API, postDataObject, token).then(
+        (data) => data.link
+      );
 
-  async function getLink() {
-    var link = await postURLData(SHORTEN_API, postDataObject, token).then(data => data.link);
-    console.log("getLink: Link is " + link);
-    return link;
+      return link;
+    }
+    var shortLink = getLink();
+    logger(shortLink);
+    // console.log(shortLink);
+    return shortLink;
+  } catch (e) {
+    logger(e);
   }
-  var shortLink = getLink();
-
-  console.log(shortLink);
-  return shortLink;
-
 }
 
-async function postURLData(url = '', data = { long_url: ' ' }, token = '') {
+async function postURLData(url = "", data = { long_url: " " }, token = "") {
   const requestObject = {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Authorization': `Bearer ${token}`,
-      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(data) // body data type must match "Content-Type" header
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
   };
 
-  const response = await fetch(url, requestObject).catch((error) => { console.log(error.message); });
-
+  const response = await fetch(url, requestObject).catch((error) => {
+    console.log(error.message);
+  });
 
   return response.json();
-
 }
 
-
-
-
-
 const APIHandler = {
-  getShortenURL_POST
+  getShortenURL_POST,
 };
 
 export default APIHandler;
-
