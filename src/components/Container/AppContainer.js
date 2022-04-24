3f60c8e1888d599214a7e90ddc04a700c897b935
Auroiah Morgan <amwebdev@outlook.com>
2022-04-23 20:05:19 -0400

    Update Gets Access Token from Bitly for App

--- src/components/Container/AppContainer.js
+++ src/components/Container/AppContainer.js
@@ -1,8 +1,10 @@
 import Header from "../Header/Header.js";
 import URLComponent from "../URLComponent/URLComponent.js";
 import Container from "react-bootstrap/Container";
+import AuthorizeUser from "../Util/Auth.js";
 
 export default function AppContainer(props) {
+  // AuthorizeUser("auroiah.morgan@gmail.com", "S@xAF0n317");
   return (
     <Container className="container container-sm" fluid>
       <Header title="URL Shortner" />
---
+++ src/components/Util/Auth.js
@@ -0,0 +1,41 @@
+import env from "react-dotenv";
+import { logger } from "./LoggerUtil";
+const BASE64 = require("base-64");
+const UTF8 = require("utf8");
+
+export default function AuthorizeUser(username, password, isLive = true) {
+  const autGranthHeaders = new Headers();
+  const encodedParams = env.CLIENT_ID_SECRET_BASE64;
+  const HOST = "api-ssl.bitly.com";
+
+  autGranthHeaders.append("Authorization", `Basic ${encodedParams}`);
+  autGranthHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+  autGranthHeaders.append("client_id", `${env.CLIENT_ID}`);
+  autGranthHeaders.append("client_secret", `${env.CLIENT_SECRET}`);
+  autGranthHeaders.append("Host", `${HOST}`);
+
+  var urlencoded = new URLSearchParams();
+  urlencoded.append("grant_type", "password");
+  urlencoded.append("username", `${username}`);
+  urlencoded.append("password", `${password}`);
+
+  var requestOptions = {
+    method: "POST",
+    headers: autGranthHeaders,
+    body: urlencoded,
+    redirect: "follow",
+  };
+  if (!isLive) {
+    console.log(encodedParams + "\n" + autGranthHeaders);
+
+    return;
+  }
+
+  fetch("https://api-ssl.bitly.com/oauth/access_token", requestOptions)
+    .then((response) => response.text())
+    .then((result) => {
+      const value = JSON.parse(result);
+      return value["access_token"];
+    })
+    .catch((error) => console.log("error", error));
+}
